cc0a00624beadb1ea08e20994d7a8a9f11266e45
apsilva1973 <apsilva1973@outlook.com>
2020-01-27 16:47:17 -0300

    evolução do projeto , conexão com o DB e Entity FramWork

---
+++ BlogPessoal.Web/BlogPessoal.Web/Controllers/CategoriasDeArtigoController.cs
@@ -0,0 +1,24 @@
+﻿using BlogPessoal.Web.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogPessoal.Web.Controllers
+{
+    public class CategoriasDeArtigoController : Controller
+    {
+
+        private BlogPessoalContexto _ctx = new BlogPessoalContexto();
+        // GET: CategoriasDeArtigo
+        public ActionResult Index()
+        {
+            var categorias = _ctx.CategoriaDeArtigo
+                .OrderBy(t => t.Nome)
+                .ToList();
+
+            return View(categorias);
+        }
+    }
+}
---
+++ BlogPessoal.Web/BlogPessoal.Web/Models/CategoriaDeArtigo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogPessoal.Web.Models
+{
+    public class CategoriaDeArtigo
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+    }
+}
